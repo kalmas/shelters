81a66932dfa5fdab7529a5a5e0401b25fd4cecae
kalmas <kylealmas@gmail.com>
2013-10-02 22:10:48 -0400

    bootstrap web interface

---
+++ web/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+function SheltersCtrl($scope) {
+  $scope.shelters = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+}
