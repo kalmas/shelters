f43157665c7b6debcd7a2e1adcd23bd236bdd114
kalmas <kylealmas@gmail.com>
2013-10-03 21:43:38 -0400

    swapped in express!

---
+++ config/express.js
@@ -0,0 +1,11 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+
+module.exports = function(server){
+
+	server.use(express.bodyParser());
+	server.use('/web', express.static(__dirname + '/../web'));
+
+};
